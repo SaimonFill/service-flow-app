b7e6d4c5326f4bd0a975ecc8f927fd988522953e
saimonfill <saimon.fill@b2breservas.com.br>
2024-07-04 14:15:30 -0300

    init project

---
+++ src/main/java/com/saimonfill/repairhistoryapi/RepairHistoryApiApplication.java
@@ -0,0 +1,13 @@
+package com.saimonfill.repairhistoryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepairHistoryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepairHistoryApiApplication.class, args);
+	}
+
+}
